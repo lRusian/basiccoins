607f4a12302159cb5a000f799a272b03fed5713d
lRusian <95121958+lRusian@users.noreply.github.com>
2022-08-15 05:16:50 -0300

    Update BasicCoins.java

--- src/main/java/com/pepsidev/lrusian/basiccoins/BasicCoins.java
+++ src/main/java/com/pepsidev/lrusian/basiccoins/BasicCoins.java
@@ -1,7 +1,7 @@
 /*
   # LRUSIAN PLUGIN CREATED AT 22/01/2022 (00h:30m AGT) END (00h:55m AGT)
   # PLUGIN CREATED FOR ALL COMMUNITY FREE CODES FOR ALL
-  # CONTACT BY DISCORD: lRussian#8326
+  # CONTACT BY DISCORD: Vuhp#6969
  */
 
 package com.pepsidev.lrusian.basiccoins;
